30c57657220851716ba668aa03dfa12068a122a1
BastienHuriaux93 <bastien.huriaux@gmail.com>
2023-06-02 14:59:34 +0200

    +contour pièces

--- PUSH/structure.cpp
+++ PUSH/structure.cpp
@@ -48,36 +48,36 @@ struct Piece {
 		{
 			pointXY = { { x1, x2, x2, x1, x1, x2, x1, x1, x2, x1, x2, x2, x3, x2, x2, x3, x3, x2, x2, x2, x5, x3, x4, x0 },
 						{ y2, y2, y4, y2, y4, y4, y1, y3, y3, y1, y1, y3, y2, y2, y1, y2, y1, y1, y4, y3, y0, y1, y1, y5 } };
-			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1, x1},
-						  { y2, y2, y4, y4, y3, y3, y1, y1, y2} };
+			contourXY = { { x1, x2, x2, x5, x2, x2, x4, x0, x3, x1, x1, x3, x3, x1 },
+						  { y2, y2, y4, y0, y3, y1, y1, y5, y1, y1, y3, y3, y4, y4 } };
 		}
 		if (type == Type::In)
 		{
 			pointXY = { { x1, x2, x2, x1, x1, x2, x2, x2, x5, x5, x2, x5 },
 						{ y2, y2, y1, y2, y1, y1, y4, y3, y4, y4, y3, y3 } };
-			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1},
-						  { y2, y2, y4, y4, y3, y3, y1, y1} };
+			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1 },
+						  { y2, y2, y4, y4, y3, y3, y1, y1 } };
 		}
 		if (type == Type::Out)
 		{
 			pointXY = { { x1, x2, x2, x1, x1, x2, x1, x2, x1, x1, x2, x2, x3, x2, x2, x3, x2, x3, x1, x3, x3, x1, x3, x3 },
 						{ y1, y3, y1, y3, y1, y3, y2, y2, y4, y4, y2, y4, y2, y2, y1, y2, y1, y1, y4, y4, y0, y3, y3, y0 } };
-			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1, x1},
-						  { y2, y2, y4, y4, y3, y3, y1, y1, y2} };
+			contourXY = { { x1, x2, x2, x1, x1, x3, x1 },
+						  { y2, y2, y1, y1, y3, y0, y4 } };
 		}
 		if (type == Type::Error)
 		{
 			pointXY = { { x1, x1, x3, x1, x3, x3, x2, x2, x4, x2, x4, x4, x1, x2, x1, x2, x2, x1, x4, x0, x4, x3, x0, x3 },
 						{ y1, y2, y2, y1, y2, y1, y2, y1, y1, y2, y2, y1, y1, y1, y4, y4, y1, y4, y2, y4, y4, y2, y4, y4 } };
-			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1, x1},
-						  { y2, y2, y4, y4, y3, y3, y1, y1, y2} };
+			contourXY = { { x1, x3, x0, x4, x2, x2, x1 },
+						  { y2, y2, y4, y2, y2, y1, y1 } };
 		}
 		if (type == Type::Tube)
 		{
 			pointXY = { { x1, x2, x2, x1, x1, x2, x1, x1, x2, x1, x2, x2, x2, x2, x3, x3, x2, x3, x1, x3, x3, x1, x3, x3, x2, x2, x5, x5, x2, x5 },
 						{ y2, y2, y4, y2, y4, y4, y1, y3, y3, y1, y1, y3, y4, y3, y4, y3, y3, y4, y3, y0, y3, y4, y0, y4, y4, y3, y4, y4, y3, y3 } };
-			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1, x1},
-						  { y2, y2, y4, y4, y3, y3, y1, y1, y2} };
+			contourXY = { { x1, x2, x2, x5, x5, x2, x2, x1, x1, x3, x1 },
+						  { y2, y2, y4, y4, y3, y3, y1, y1, y3, y0, y4 } };
 		}
 	}
 
