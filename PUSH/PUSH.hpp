51303873e31edb1a05a3990f463dab1c55af78ef
BastienHuriaux93 <bastien.huriaux@gmail.com>
2023-06-02 14:30:39 +0200

    -glad

--- PUSH/PUSH.hpp
+++ PUSH/PUSH.hpp
@@ -8,9 +8,7 @@
 #include <filesystem>
 #include <unistd.h>
 #include <GL/glew.h>
-//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
